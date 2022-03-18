8a6ce042632aae21b79225385a9ce6ec519013a4
MalasiewiczMateusz <mateusz-michal.malasiewicz@capgemini.com>
2022-03-18 15:52:55 +0100

    commit routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticuloComponent } from './articulo/articulo.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { CompraComponent } from './compra/compra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path:'',
+    component:ArticuloComponent,
+    pathMatch:'full'
+
+  },
+
+  {
+
+    path:'clientes',
+    component:ClientesComponent
+
+  },
+
+  {
+    path:'compras',
+    component:CompraComponent
+  },
+
+  {
+    path:'**',
+    redirectTo:''
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
